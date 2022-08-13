d694426a2d2f72b3a7b65840192359fbbbae343d
jbdev <bordet.julian@gmail.com>
2022-08-13 12:03:53 -0300

    initial commit cotaining springboot base files and a folder with the database schema

---
+++ src/main/java/com/jb/erebor/EreborApplication.java
@@ -0,0 +1,13 @@
+package com.jb.erebor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EreborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EreborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jb/erebor/EreborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.erebor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EreborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
